daa0d503150b4c911497ad297270a06f2113d67d
RAKHYUN <kyle2598@gmail.com>
2024-01-19 10:41:26 -0600

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Rakhyun Kim on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

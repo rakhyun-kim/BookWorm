a9ea696c6e9744bcc0d1517e51b1e5ade6708c65
RAKHYUN <kyle2598@gmail.com>
2024-01-22 15:38:35 -0600

    -Add Star image for review

---
+++ BookWorm/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  BookWorm
+//
+//  Created by Rakhyun Kim on 1/22/24.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
---
+++ BookWorm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  BookWorm
+//
+//  Created by Rakhyun Kim on 1/22/24.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
